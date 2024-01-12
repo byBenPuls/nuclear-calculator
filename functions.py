2a383e5f3e153921acd11b232e4b62a5b5fe8d5c
Ben Puls <61308126+byBenPuls@users.noreply.github.com>
2024-01-12 22:50:19 +0500

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,6 +1,16 @@
-def reaction_energy(e1, e2, convertationconst):
-    return (e1 - e2) * convertationconst
+from math import pow, fsum
+
+
+def reaction_energy(e1, e2, e3, e4, convertationconst):
+    return (fsum([e1, e2]) - fsum([e3, e4])) * convertationconst
+# Measured in MeV
 
 
 def threshold_energy(q, e1, e2):
     return abs(q) * (1 + (e1 / e2))
+# Measured in MeV
+
+
+def nucleus_radius(const, mass_number):
+    return const * pow(mass_number, 1 / 3)
+# Measured in Fm
--- main.py
+++ main.py
@@ -1,45 +1,46 @@
-from math import fsum
-from data import elements_dict, electron_mass, elem_part, const
+from data import elements_dict, electron_mass, elements_particle, const, radius_const
 from functions import reaction_energy, threshold_energy
 
-a = input("Enter your element (A): ").upper()
-b = input("Enter your element (B): ").upper()
-c = input("Enter your element (C): ").upper()
-d = input("Enter your element (D): ").upper()
 
-if (a and b and c and d) in elements_dict or elem_part:
-    if {a, b, c, d} <= set(elem_part.keys()) | set(elements_dict.keys()):
-        if a in set(elem_part.keys()):
-            r1 = elem_part[a]
-        else:
-            element1 = elements_dict[a]
-            r1 = element1[1] - element1[0] * electron_mass
-    if b in set(elem_part.keys()):
-        r2 = elem_part[b]
+print('Reaction like this: A + B => C + D\n')
+a = input(f"Enter your element (A): ").upper()
+b = input(f"Enter your element (B): ").upper()
+c = input(f"Enter your element (C): ").upper()
+d = input(f"Enter your element (D): ").upper()
+
+if {a, b, c, d} <= set(elements_particle.keys()) | set(elements_dict.keys()):
+    if a in set(elements_particle.keys()):
+        r1 = elements_particle[a]
+    else:
+        element1 = elements_dict[a]
+        r1 = element1[1] - element1[0] * electron_mass
+    if b in set(elements_particle.keys()):
+        r2 = elements_particle[b]
     else:
         element2 = elements_dict[b]
         r2 = element2[1] - element2[0] * electron_mass
-    if c in set(elem_part.keys()):
-        r3 = elem_part[c]
+    if c in set(elements_particle.keys()):
+        r3 = elements_particle[c]
     else:
         element3 = elements_dict[c]
         r3 = element3[1] - element3[0] * electron_mass
-    if d in set(elem_part.keys()):
-        r4 = elem_part[d]
+    if d in set(elements_particle.keys()):
+        r4 = elements_particle[d]
     else:
         element4 = elements_dict[d]
         r4 = element4[1] - element4[0] * electron_mass
-    e = reaction_energy(fsum([r1, r2]), fsum([r3, r4]), const)
+    e = reaction_energy(r1, r2, r3, r4, const)
     t = threshold_energy(e, r1, r2)
     if e < 0:
-        input(f'Reaction {a.lower()} + {b.lower()} => {c.lower()} + {d.lower()} is edonthermic.\n'
-              f'Energy yield reaction: {e:.5f} MeV.\n'
+        input(f'\nReaction {a.lower()} + {b.lower()} => {c.lower()} + {d.lower()} '
+              f'is endoenergetic.\n\n'
+              f'Energy yield reaction: {e:.5f} MeV\n'
               f'Threshold energy of bombarding particle ({a.lower()}): {t:.5f} MeV.'
-              f'\nPress any key to continue...')
+              f'\n\n\nPress any key to continue...')
     if e > 0:
-        input(f'Реакция {a.lower()} + {b.lower()} => {c.lower()} + {d.lower()} is exothermic.\n'
-              f'Energy yield reaction: {e:.5f} MeV.\n'
-              f'Threshold energy of bombarding particle ({a.lower()}): {t:.5f} MeV.'
-              f'\nPress any key to continue...')
+        input(f'\nReaction {a.lower()} + {b.lower()} => {c.lower()} + {d.lower()} '
+              f'is exoenergetic.\n\n'
+              f'Energy yield reaction: {e:.5f} MeV.'
+              f'\n\n\nPress any key to continue...')
 else:
     input('Element not found...')
