753e5c081ec231b02a063f367214aedab0906c93
Ben Puls <61308126+byBenPuls@users.noreply.github.com>
2024-01-17 20:10:14 +0500

    Add files via upload

--- js-bot/main.js
+++ js-bot/main.js
@@ -1,1 +1,9 @@
-
+const db = require('./file.json')
+const elem = require('./elements.json')
+let array = 'Og-293'
+array = array.split('-', 3)
+console.log(array)
+const q = '1'
+// db[118-1].isotopes
+const mass = array[0]
+console.log(db[elem.H - 1])
